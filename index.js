518d59ba67f965e557b73e15344325ba373402de
Igor Chigur <chigur.ig@gmail.com>
2019-07-10 14:15:12 +0300

    fix

--- index.js
+++ index.js
@@ -184,9 +184,9 @@ function getEvents(id,res){
 
 
 app.post('/registration',(req,res)=>{
-    // res.header('Access-Control-Allow-Origin', '*');
-    // res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // res.header('Access-Control-Allow-Headers', "*");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', "*");
     let gender="";
     let name="";
     let surname="";
