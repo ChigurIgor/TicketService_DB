273554a946f4adba0c5faecb7cc6636708c774fb
Igor Chigur <chigur.ig@gmail.com>
2019-08-27 16:32:15 +0300

    fix

--- index.js
+++ index.js
@@ -914,8 +914,10 @@ function generatePdf(docDefinition, callback) {
     try {
         const fontDescriptors = {
             Roboto: {
-                normal: path.resolve('src', 'fonts', 'Roboto.ttf'),
-                bold: path.resolve('src', 'fonts', 'Roboto-Bold.ttf'),
+                normal: 'node_modules/roboto-font/fonts/Roboto/roboto-regular-webfont.ttf',
+                bold: 'node_modules/roboto-font/fonts/Roboto/roboto-bold-webfont.ttf',
+                italics: 'node_modules/roboto-font/fonts/Roboto/roboto-italic-webfont.ttf',
+                bolditalics: 'node_modules/roboto-font/fonts/Roboto/roboto-bolditalic-webfont.ttf'
             }
         };
 
