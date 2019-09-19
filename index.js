fca38d625f7d36e1468d4d8a80902c3b39b0d954
Igor Chigur <chigur.ig@gmail.com>
2019-09-19 11:40:28 +0300

    fix

--- index.js
+++ index.js
@@ -955,7 +955,7 @@ function sendPDF(documents, res) {
     let content = [];
     content.push('This is your order id:'+123+ '\n ');
     for( let plase of documents[0].places){
-        content.push(plase)
+        content.push("row: "+ plase.row + ", seat: "+plase.seat+" \n");
     }
     const docDefinition = {
         // content: ['This is your order id:'+123+ '\n next \n next']
